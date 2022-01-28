863bf29b2eb288db3994282fd7222049f02dd874
Sandy Cash <lhcash@us.ibm.com>
2022-01-28 15:21:58 -0500

    Implement addition, negation, subtraction of matrices

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -19,6 +19,45 @@ func NewMatrix(rows, cols int) Matrix {
 	return m
 }
 
+// Negate returns the negative of the specified matrix A,
+// which is a matrix -A in which each element has been multiplied
+// by -1.
+func Negate(mA Matrix) Matrix {
+	mAneg := NewMatrix(len(mA), len(mA[0]))
+
+	for rn := range mAneg {
+		for cn := range mAneg[rn] {
+			mAneg[rn][cn] = (-1) * mA[rn][cn]
+		}
+	}
+
+	return mAneg
+}
+
+// Add adds two r x c matrices, errors if dimensions are not the same.
+func Add(mA, mB Matrix) (Matrix, error) {
+	if len(mA) != len(mB) || len(mA[0]) != len(mB[0]) {
+		return nil, errors.New("Cannot add differently sized matrices")
+	}
+
+	mC := NewMatrix(len(mA), len(mA[0]))
+
+	for rn := range mA {
+		for cn := range mA[rn] {
+			mC[rn][cn] = mA[rn][cn] + mB[rn][cn]
+		}
+	}
+
+	return mC, nil
+}
+
+// Subtract subtracts matrix mB from matrix mA.  This is implemented by
+// adding mA and -mB at the cost of a constant multiplier, but we gain
+// reuse and code simplicity.
+func Subtract(mA, mB Matrix) (Matrix, error) {
+	return Add(mA, Negate(mB))
+}
+
 // Multiply implements simple brute-force matrix multiplication
 // of two compatible matrices.  Incompatibility results in an
 // error being returned.
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -10,6 +10,7 @@ import (
 var _ = Describe("Matrix", func() {
 	var (
 		rows, cols int
+		mA, mB     matrix.Matrix
 	)
 
 	Describe("NewMatrix", func() {
@@ -27,9 +28,91 @@ var _ = Describe("Matrix", func() {
 		})
 	})
 
+	Describe("Negate", func() {
+		BeforeEach(func() {
+			rows = 4
+			cols = 4
+			mA = matrix.NewMatrix(rows, cols)
+			for rn := range mA {
+				for cn := range mA[rn] {
+					mA[rn][cn] = rn * cn
+				}
+			}
+		})
+
+		It("Negates each element of the matrix", func() {
+			mANeg := matrix.Negate(mA)
+			Expect(len(mANeg)).To(Equal(rows))
+			for rn, row := range mANeg {
+				Expect(len(row)).To(Equal(cols))
+				for cn := range mANeg[rn] {
+					Expect(mANeg[rn][cn]).To(Equal(cn * rn * (-1)))
+				}
+			}
+		})
+	})
+
+	Describe("Add", func() {
+		BeforeEach(func() {
+			rows = 4
+			cols = 5
+
+			mA = matrix.NewMatrix(rows, cols)
+			mB = matrix.NewMatrix(rows, cols)
+
+			for rn := 0; rn < rows; rn++ {
+				for cn := 0; cn < cols; cn++ {
+					mA[rn][cn] = rn + cn
+					mB[rn][cn] = 8 - (rn + cn)
+				}
+			}
+		})
+
+		It("Adds the two matrices correctly", func() {
+			mC, err := matrix.Add(mA, mB)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(mC)).To(Equal(4))
+			for _, row := range mC {
+				Expect(row).To(HaveLen(cols))
+
+				for _, c := range row {
+					Expect(c).To(Equal(8))
+				}
+			}
+		})
+	})
+
+	Describe("Subtract", func() {
+		BeforeEach(func() {
+			rows = 4
+			cols = 4
+			mA = matrix.NewMatrix(rows, cols)
+			mB = matrix.NewMatrix(rows, cols)
+
+			for rn := 0; rn < rows; rn++ {
+				for cn := 0; cn < cols; cn++ {
+					mA[rn][cn] = rn * cn
+					mB[rn][cn] = rn + cn
+				}
+			}
+		})
+
+		It("Subtracts the two matrices correctly", func() {
+			mC, err := matrix.Subtract(mA, mB)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(mC)).To(Equal(rows))
+			for rn, row := range mC {
+				Expect(len(row)).To(Equal(cols))
+
+				for cn, v := range row {
+					Expect(v).To(Equal((rn * cn) + ((rn + cn) * (-1))))
+				}
+			}
+		})
+	})
+
 	Describe("Multiply", func() {
 		var (
-			mA, mB                     matrix.Matrix
 			rowsA, colsA, rowsB, colsB int
 		)
 
