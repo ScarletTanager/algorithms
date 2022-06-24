bcb74c4cd50a2ce1a7db56a98281231f8c823c75
Sandy Cash <lhcash@us.ibm.com>
2022-06-24 09:49:51 -0400

    Add documentation for heapsort

--- heap/heap_sort.go
+++ heap/heap_sort.go
@@ -2,6 +2,14 @@ package heap
 
 import "golang.org/x/exp/constraints"
 
+// Sort implements heapsort.  The slice passed in is sorted in situ, and the
+// function returns the sorted slice as well.
+// Heapsort works by moving the current topmost node to the end - in a max
+// heap, this node is always guaranteed to be the largest value - and then truncating
+// the heap before re-maxifying from the top.  This brings the next largest
+// value up to the topmost node, which is then swapped with the last node in the
+// heap (this is why we truncate after every swap).
+// Heapsort runs in Θ(nlogn).
 func Sort[O constraints.Ordered](d []O) []O {
 	h := NewMaxHeap(d)
 	for i := h.length; i >= 2; i-- {
